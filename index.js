025b4ead837387f549d0b6c477cdd6e6ab081850
Maximilian Zollneritsch <m.zollneritsch@netconomy.net>
2018-01-25 21:20:12 +0100

    Added current rules from ongoing project: * curly * quotes * camelcase * prefer-template * prefer-const * nodejs / globals * react/sort-comp * react/no-danger * react/display-name Refactored code to fit rules.

--- index.js
+++ index.js
@@ -3,72 +3,88 @@ module.exports = {
         ///////////////////////////////////////////////////////////////////////
         // Possible Errors
         ///////////////////////////////////////////////////////////////////////
-        "no-extra-boolean-cast": 1,
-        "comma-dangle": [2, "never"],
-        "no-empty": 1,
-        "no-extra-parens": [2, "functions"],
+        'no-extra-boolean-cast': 1,
+        'comma-dangle': [2, 'never'],
+        'no-empty': 1,
+        'no-extra-parens': [2, 'functions'],
 
         ///////////////////////////////////////////////////////////////////////
         // Best Practices
         ///////////////////////////////////////////////////////////////////////
-        "no-unused-expressions": 1,
-        "no-multi-spaces": 2,
-        "curly": 2,
-        "eqeqeq": 2,
-        "guard-for-in": 2,
-        "wrap-iife": 0,
-        "no-caller": 2,
-        "no-new": 2,
-
+        'no-unused-expressions': 1,
+        'no-multi-spaces': 2,
+        'curly': [
+            1,
+            'all'
+        ],
+        'eqeqeq': 2,
+        'guard-for-in': 2,
+        'wrap-iife': 0,
+        'no-caller': 2,
+        'no-new': 2,
         ///////////////////////////////////////////////////////////////////////
         // Stylistic Issues
         ///////////////////////////////////////////////////////////////////////
-        "no-underscore-dangle": 0,
-        "camelcase": 0,
-        "new-cap": 2,
-        "quotes": 0,
-        "no-trailing-spaces": 2,
-        "key-spacing": [1, {
+        'no-underscore-dangle': 0,
+        'camelcase': [
+            2,
+            {
+                'properties': 'never'
+            }
+        ],
+        'new-cap': 2,
+        'quotes': [
+            1,
+            'single',
+            {
+                'avoidEscape': true,
+                'allowTemplateLiterals': true
+            }
+        ],
+        'no-trailing-spaces': 2,
+        'key-spacing': [1, {
             beforeColon: false,
             afterColon: true
         }],
-        "indent": [2, 4],
-        "brace-style": [2, "1tbs"],
-        "comma-spacing": [2, {before: false, after: true}],
-        "comma-style": [2, "last"],
-        "consistent-this": [1, "self"],
-        "eol-last": 1,
-        "new-parens": 2,
-        "no-array-constructor": 2,
-        "no-mixed-spaces-and-tabs": 2,
-        "no-multiple-empty-lines": 2,
-        "no-spaced-func": 1,
-        "padded-blocks": [2, "never"],
-        "semi": [2, "always"],
-        "keyword-spacing": [2, {"after": true}],
-        "space-infix-ops": 2,
-        "array-bracket-spacing": [2, "never"],
-        "object-curly-spacing": [2, "never"],
+        'indent': [2, 4],
+        'brace-style': [2, '1tbs'],
+        'comma-spacing': [2, {before: false, after: true}],
+        'comma-style': [2, 'last'],
+        'consistent-this': [1, 'self'],
+        'eol-last': 1,
+        'new-parens': 2,
+        'no-array-constructor': 2,
+        'no-mixed-spaces-and-tabs': 2,
+        'no-multiple-empty-lines': 2,
+        'no-spaced-func': 1,
+        'padded-blocks': [2, 'never'],
+        'semi': [2, 'always'],
+        'keyword-spacing': [2, {'after': true}],
+        'space-infix-ops': 2,
+        'array-bracket-spacing': [2, 'never'],
+        'object-curly-spacing': [2, 'never'],
+        'prefer-template': 1,
 
         ///////////////////////////////////////////////////////////////////////
         // Variables
         ///////////////////////////////////////////////////////////////////////
-        "no-use-before-define": [1, "nofunc"],
-        "no-unused-vars": 1,
+        'no-use-before-define': [1, 'nofunc'],
+        'no-unused-vars': 1,
         // Undefined variables should be treated as errors:
-        "no-undef": 2,
+        'no-undef': 2,
+        'prefer-const': 1,
 
         ///////////////////////////////////////////////////////////////////////
         // Strict Mode
         ///////////////////////////////////////////////////////////////////////
-        "strict": 0,
+        'strict': 0,
 
         ///////////////////////////////////////////////////////////////////////
         // Legacy
         ///////////////////////////////////////////////////////////////////////
-        "no-bitwise": 1,
-        "no-plusplus": 0,
-        "max-params": [2, 7],
-        "max-len" : [1, 120]
+        'no-bitwise': 1,
+        'no-plusplus': 0,
+        'max-params': [2, 7],
+        'max-len': [1, 120]
     }
 };
--- react.js
+++ react.js
@@ -1,20 +1,54 @@
 module.exports = {
-    extends: ["./index.js", "plugin:react/recommended"],
+    extends: ['./index.js', 'plugin:react/recommended'],
     env: {
         browser: true
     },
     plugins: [
-        "react"
+        'react'
     ],
     parserOptions: {
         sourceType: 'module',
         ecmaVersion: 6,
         ecmaFeatures: {
-            "jsx": true
+            'jsx': true
         }
     },
     rules: {
-        "react/display-name": 0,
-        "react/no-danger": 0
+        'react/display-name': 0,
+        'react/no-danger': 0,
+        'react/sort-comp': [
+            2,
+            {
+                'order': [
+                    'lifecycle',
+                    'everything-else',
+                    'render'
+                ],
+                'groups': {
+                    'lifecycle': [
+                        'mixins',
+                        'propTypes',
+                        'contextTypes',
+                        'childContextTypes',
+                        'defaultProps',
+                        'constructor',
+                        'getDefaultProps',
+                        'getInitialState',
+                        'getChildContext',
+                        'componentWillMount',
+                        'componentDidMount',
+                        'componentWillReceiveProps',
+                        'shouldComponentUpdate',
+                        'componentWillUpdate',
+                        'componentDidUpdate',
+                        'componentWillUnmount'
+                    ]
+                }
+            }
+        ],
+        'react/prop-types': 1,
+        'react/no-did-mount-set-state': 1,
+        'react/no-did-update-set-state': 1,
+        'react/no-string-refs': 1
     }
 };
